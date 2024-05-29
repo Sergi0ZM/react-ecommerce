b1eb9bac687aaefb936a4fa364e746dadc08b0e5
Sergi0ZM <sergioazm98@gmail.com>
2024-05-29 18:22:55 -0500

    Create project

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+      <div className='bg-red-200'>
+        Hola
+      </div>
+     
+    </>
+  )
+}
+
+export default App
